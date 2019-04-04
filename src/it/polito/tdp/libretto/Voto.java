1a06d7af3b4283d66d669c7fe6d640291288dc5a
Caso <Caso@10.13.14.54>
2019-04-04 17:45:05 +0200

    Progetto iniziale. Classe Voto vuota.

---
+++ src/it/polito/tdp/libretto/Voto.java
@@ -0,0 +1,12 @@
+package it.polito.tdp.libretto;
+
+import java.time.LocalDate;
+
+public class Voto {
+	private int voto ;
+	private String corso ;
+	private LocalDate data ; //la data si rappresentava con l'oggetto Date o Calendar fino a java 6,a partire da java 7 utilizziamo la libreria java.date che mi permette di usare localdate
+	
+	
+
+}
